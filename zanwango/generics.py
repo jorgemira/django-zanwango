2f272b9f5031b98996b98237416bff7e1485f19c
JorgeMira <jorge.mira.yague@gmail.com>
2024-09-19 13:37:56 +0200

    feat: Add generics to support write operations

--- zanwango/generics.py
+++ zanwango/generics.py
@@ -1,3 +1,4 @@
+from django.db import models
 from rest_framework import generics
 
 from zanwango import schemas
@@ -8,3 +9,21 @@ class GenericReadAPIView(generics.GenericAPIView):
 
     def get_schema_out(self) -> type[schemas.Schema] | None:
         return self.schema_out
+
+
+class GenericWriteAPIView(generics.GenericAPIView):
+    schema_in: type[schemas.Schema] | None = None
+    model: type[models.Model] | None = None
+
+    def get_schema_in(self) -> type[schemas.Schema] | None:
+        return self.schema_in
+
+    def get_model(self) -> type[models.Model]:
+        if self.model is not None:
+            return self.model
+        model: type[models.Model] = self.get_queryset().model
+        return model
+
+
+class GenericAPIView(GenericReadAPIView, GenericWriteAPIView):
+    pass
--- zanwango/viewsets.py
+++ zanwango/viewsets.py
@@ -7,6 +7,14 @@ class GenericReadViewSet(viewsets.ViewSetMixin, generics.GenericReadAPIView):
     pass
 
 
+class GenericWriteViewSet(viewsets.ViewSetMixin, generics.GenericWriteAPIView):
+    pass
+
+
+class GenericViewSet(viewsets.ViewSetMixin, generics.GenericAPIView):
+    pass
+
+
 class ReadOnlyModelViewSet(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericReadViewSet
 ):
