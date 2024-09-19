910f95b22e6159cd6e8d010c130d082421b8f11c
JorgeMira <jorge.mira.yague@gmail.com>
2024-09-19 13:40:09 +0200

    fix: Refactored mixins.py
    - Alias import
    - Refactor variable names for consistency
    - Fix typo causing bug

--- zanwango/mixins.py
+++ zanwango/mixins.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from pydantic import TypeAdapter
 from rest_framework import mixins
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView as DRFGenericAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -11,32 +11,33 @@ from zanwango.generics import GenericReadAPIView
 
 class RetrieveModelMixin(mixins.RetrieveModelMixin):
     def retrieve(
-        self: GenericAPIView, request: Request, *args: Any, **kwargs: Any
+        self: DRFGenericAPIView, request: Request, *args: Any, **kwargs: Any
     ) -> Response:
         if isinstance(self, GenericReadAPIView):
             if schema_out := self.get_schema_out():
                 instance = self.get_object()
-                model = schema_out.model_validate(instance)
-                return Response(model.model_dump())
+                model_dump = schema_out.model_validate(instance).model_dump()
+                return Response(model_dump)
 
         return super().retrieve(request, *args, **kwargs)
 
 
 class ListModelMixin(mixins.ListModelMixin):
     def list(
-        self: GenericAPIView, request: Request, *args: Any, **kwargs: Any
+        self: DRFGenericAPIView, request: Request, *args: Any, **kwargs: Any
     ) -> Response:
-        if isinstance(self, GenericReadAPIView) and ():
+        if isinstance(self, GenericReadAPIView):
             if schema_out := self.get_schema_out():
                 type_adapter = TypeAdapter(list[schema_out])  # type: ignore[valid-type]
                 queryset = self.filter_queryset(self.get_queryset())
 
                 if (page := self.paginate_queryset(queryset)) is not None:
                     models = type_adapter.validate_python(page)
-                    dumped_models = [model.model_dump() for model in models]
-                    return self.get_paginated_response(dumped_models)
+                    model_dumps = [model.model_dump() for model in models]
+                    return self.get_paginated_response(model_dumps)
 
                 models = type_adapter.validate_python(queryset)
-                return Response([model.model_dump() for model in models])
+                model_dumps = [model.model_dump() for model in models]
+                return Response(model_dumps)
 
         return super().list(request, *args, **kwargs)
